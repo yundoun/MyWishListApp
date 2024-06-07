db913f843d12751a316b8e336b2cfe8d990849ba
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2024-06-07 11:14:45 +0900

    Feat : FloatingActionButton

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -4,10 +4,16 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView() {
@@ -17,11 +23,24 @@ fun HomeView() {
             AppBarView(title = "WishList") {
                 Toast.makeText(context, "뒤로가기 버튼 클릭", Toast.LENGTH_LONG).show()
             }
+        },
+
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 20.dp),
+                contentColor = Color.White,
+                backgroundColor = Color.Black,
+                onClick = {  /*화면 추가 및 화면 수정*/ }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
+            }
+
         }
     ) {
-        LazyColumn(modifier = Modifier
-            .fillMaxSize()
-            .padding(it)) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
 
         }
     }
